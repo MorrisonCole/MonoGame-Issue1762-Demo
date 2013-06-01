f137d3effabe121453b46909417d2af7c0c4b8ec
Morrison Cole <admin@morrisoncole.co.uk>
2013-06-01 07:49:17 +0100

    Added simple null reference demonstration.

---
+++ GraphicsDeviceTest/MyGame.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace GraphicsDeviceTest
+{
+    using System.Diagnostics;
+
+    class MyGame : Game
+    {
+        public MyGame()
+        {
+            var graphicsDeviceManager = new GraphicsDeviceManager(this);
+            graphicsDeviceManager.ApplyChanges();
+
+            Debug.Assert(graphicsDeviceManager.GraphicsDevice != null);
+        }
+    }
+}
---
+++ GraphicsDeviceTest/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GraphicsDeviceTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var myGame = new MyGame())
+            {
+                myGame.Run();
+            }
+        }
+    }
+}
